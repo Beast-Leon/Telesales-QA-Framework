57ea904141f64bb47f03b0297431f5843d395469
leonlu233 <903433053@qq.com>
2022-08-18 13:35:15 +0800

    Add more test cases

--- helper/lexicons.py
+++ helper/lexicons.py
@@ -1,9 +1,9 @@
 # classes with respective sample sentences
-greeting_lexicons = {'opening': ['good time to talk', 'good morning', 'good afternoon', 'good evening', 'calling from income', 'hello i am looking for mister', 'may i speak with', 'calling for the number'],
+greeting_lexicons = {'opening': ['good time to talk', 'good morning', 'good afternoon', 'good evening', 'calling from income', 'hello i am looking for mister', 'may i speak with', 'calling for the number', 'this is agent from income ntuc'],
                     'purpose_of_call': ['the purpose of my call is to inform you', 'as a valued customer of income', 'we have designed this plan called', 'this plan is for exclusive customers like yourself', 'we formulated a special fiftieth anniversary insurance bundle called the i fifty', 'we have this anniversary plan for the family'],
                     'ask_for_permission': ['how may i address you', 'may i call you', 'is it a good time to talk to you', 'are you convenient to talk']}
 
 ending_lexicons = {'closing': ['thank you so much for your time', 'have a great time', 'have a nice day', 'have a good day', 'okay then bye thanks', 'thank you bye bye', 'bye bye', 'wish you a good day', 'thank you for insuring with income', 'please do not hesitate to contact me', 'no problem thank you', 'anything else i can assist you with', 'thank you for your time', 'xie xie'],
-                   'data_enrichment': ['what is your email address'],
+                   'data_enrichment': ['what is your email address', 'am i pronuncing your name correctly', 'do you have any other questions'],
                   'follow-up': ['can i contact you later in the afternoon', 'i will call you at four or five', 'i will call you back on monday', 'you prefer morning or afternoon', 'i will do a follow up call next week', 'i will drop you an email', 'i will call you at another time', 'you can give me a call at this number', 'you can give us a call back']}
 
